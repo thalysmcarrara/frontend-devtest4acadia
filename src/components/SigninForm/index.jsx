a16d1f81c8522466e7cbb13c19bcddff0e770ad2
thalysmcarrara <thalysmatiascarrara@gmail.com>
2021-10-01 23:04:32 -0400

    login form finished

--- src/components/SigninForm/index.jsx
+++ src/components/SigninForm/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 import close from '../../assets/close.svg';
+import closeAlert from '../../assets/closealert.svg';
 import './index.css';
 
 const initialValue = {
@@ -10,6 +11,7 @@ const initialValue = {
 };
 
 export default function SigninForm() {
+  const [error, setError] = useState('');
   const [formData, setFormData] = useState(initialValue);
   const history = useHistory();
 
@@ -20,13 +22,24 @@ export default function SigninForm() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    axios.post('localhost:3001/login', formData).then((response) => {
+    axios.post('https://apidevtest4cadia.herokuapp.com/login', formData).then((response) => {
       console.log(response);
+      const { token } = response.data;
+      localStorage.setItem('token', token);
+      history.push('/dashboard');
+    }).catch((err) => {
+      console.log(err.response);
+      const { message } = err.response.data;
+      setError(message);
     });
   };
 
   const handleClose = () => {
     history.push('/');
+  };
+
+  const handleCloseAlert = () => {
+    setError('');
   };
 
   return (
@@ -40,7 +53,23 @@ export default function SigninForm() {
       </button>
       <div className="content">
         <h1>SignIn</h1>
-        <div className="alert" />
+
+        <div className={ error !== '' ? 'unfill' : 'fill' } />
+
+        <div
+          className="alert"
+          style={ { display: `${error !== '' ? 'block' : 'none'}` } }
+        >
+          <button
+            id="button-close"
+            type="button"
+            onClick={ handleCloseAlert }
+          >
+            <img className="close-alert" src={ closeAlert } alt="close alert" />
+          </button>
+          {error !== '' ? error : '' }
+        </div>
+
         <form onSubmit={ handleSubmit }>
           <label htmlFor="email">
             <input
@@ -60,6 +89,7 @@ export default function SigninForm() {
               type="password"
               name="password"
               id="password"
+              minLength="8"
               placeholder="Password"
             />
           </label>
